15e81b7421991b3af8e0a8ae17911d6e55d09af1
snirkol <snirkol@gmail.com>
2017-08-26 19:27:43 +0300

    unitest

---
+++ src/main/test/com/hit/algorithm/IAlgoCacheTest.java
@@ -0,0 +1,13 @@
+package com.hit.algorithm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IAlgoCacheTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
